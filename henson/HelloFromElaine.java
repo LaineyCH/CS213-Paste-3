258010f628ade9d6321ade0beaadb25cf11ec99b
clee999 <64559134+clee999@users.noreply.github.com>
2021-05-11 23:26:47 +0100

    Adding another sentence .

--- henson/HelloFromElaine.java
+++ henson/HelloFromElaine.java
@@ -5,6 +5,6 @@ public class HelloFromElaine {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, my name is Elaine.");
+        System.out.println("Hello, my name is Elaine. My business is called Elaine's Sweet Life.");
     }
 }
