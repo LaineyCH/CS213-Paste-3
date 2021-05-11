e7d834d5fe34564089820487d824b28eecf7fe97
Elaine Henson <hen20028@byui.edu>
2021-05-11 16:13:53 +0100

    Remove surname

--- henson/HelloFromElaine.java
+++ henson/HelloFromElaine.java
@@ -5,6 +5,6 @@ public class HelloFromElaine {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, my name is Elaine Henson.");
+        System.out.println("Hello, my name is Elaine.");
     }
 }
