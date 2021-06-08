e3c355a0f0cec09c4cfdb2a512c414cb396b9a35
Elaine Henson <hen20028@byui.edu>
2021-06-08 19:54:13 +0100

    Adding basic database read and write functions

--- app/src/main/java/paste3/bakersbox/MainActivity.java
+++ app/src/main/java/paste3/bakersbox/MainActivity.java
@@ -1,14 +1,54 @@
 package paste3.bakersbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create an instance of the Firebase Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance(
+                "https://baker-s-box-default-rtdb.europe-west1.firebasedatabase.app/");
+        myRef = database.getReference("message");
+    }
+
+    public void basicWrite(View view) {
+
+        // Write a message to the database
+        myRef.setValue("Hello Jonathan");
+
+    }
+
+    public void basicRead(View view) {
+
+        myRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
     }
 }
