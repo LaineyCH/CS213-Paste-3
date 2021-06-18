3119461d43c49a7521e5fb5cf3bd4cf1150c558b
clee999 <64559134+clee999@users.noreply.github.com>
2021-06-18 16:41:17 +0100

    adding search view classes

---
+++ app/src/main/java/paste3/bakersbox/List_view_items.java
@@ -0,0 +1,14 @@
+package paste3.bakersbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class List_view_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_items);
+    }
+}
