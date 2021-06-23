72547de85b7daca31db88507477eb7af5bc11165
Spencer-26p <spencer1997@byui.edu>
2021-06-23 13:34:18 -0500

    -added EditIngredientActivity,EditRecipeActivity,IngredientsActivity,ViewRecipeActivity,RecipeActivity,and edited main. -added RecipeIngredient and Recipe classes.

--- app/src/main/java/paste3/bakersbox/IngredientsActivity.java
+++ app/src/main/java/paste3/bakersbox/IngredientsActivity.java
@@ -8,23 +8,17 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class IngredientsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ConstraintLayout li=(ConstraintLayout)findViewById(R.id.layout1);
+        setContentView(R.layout.activity_ingredients);
+        ConstraintLayout li=(ConstraintLayout)findViewById(R.id.layout3);
         li.setBackgroundColor(Color.parseColor("#7BEEE4"));
     }
-
-    public void goToRecipes(View view){
-        Intent intent = new Intent(this,RecipeActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToIngredients(View view){
-        Intent intent = new Intent(this,IngredientsActivity.class);
+    public void goToPurchase(View view){
+        Intent intent = new Intent(this,EditIngredientActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/paste3/bakersbox/RecipeActivity.java
+++ app/src/main/java/paste3/bakersbox/RecipeActivity.java
@@ -7,24 +7,21 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+
+public class RecipeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ConstraintLayout li=(ConstraintLayout)findViewById(R.id.layout1);
+        setContentView(R.layout.activity_recipe2);
+        LinearLayout li=(LinearLayout)findViewById(R.id.layout5);
         li.setBackgroundColor(Color.parseColor("#7BEEE4"));
     }
 
-    public void goToRecipes(View view){
-        Intent intent = new Intent(this,RecipeActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToIngredients(View view){
-        Intent intent = new Intent(this,IngredientsActivity.class);
+    public void createRecipe(View view) {
+        Intent intent = new Intent(this,EditRecipeActivity.class);
         startActivity(intent);
     }
 }
