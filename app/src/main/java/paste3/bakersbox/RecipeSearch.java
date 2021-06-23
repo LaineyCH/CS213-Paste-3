013f4340d2ab1391b3e17ae09835c7ec207836c0
clee999 <64559134+clee999@users.noreply.github.com>
2021-06-23 21:21:30 +0100

    Adding Search classes and Xml layouts

---
+++ app/src/main/java/paste3/bakersbox/RecipeSearch.java
@@ -0,0 +1,64 @@
+package paste3.bakersbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.SearchView.OnQueryTextListener;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+
+
+public class RecipeSearch extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] recipeNameList;
+    ArrayList<RecipeNames> arraylist = new ArrayList<RecipeNames>();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_view_layout);
+
+        // Generate sample data
+
+        recipeNameList= new String[]{"Cherry pie", "Doughnuts", "Victoria sponge",
+                "Bakewell tarts", "Apple pie", "Lemon meringue", "Forest gateau", "Apple crumble",
+                "Rhubarb crumble","Chocolate gateau"};
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < recipeNameList.length; i++) {
+            RecipeNames recipeNames = new RecipeNames(recipeNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(recipeNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+    }
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+}
